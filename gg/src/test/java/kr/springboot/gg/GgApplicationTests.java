b5bc41966bafc11239763bf2f3576cba08f2c719
진우 <nice7677@naver.com>
2023-01-10 13:00:38 +0900

    qr 연습

---
+++ gg/src/test/java/kr/springboot/gg/GgApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.springboot.gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
