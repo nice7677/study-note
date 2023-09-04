4214379149cd19b2b85fa5443f817f08a7095b8f
진우 <nice7677@naver.com>
2023-09-04 22:59:05 +0900

    test: 테스트 코드 수정

--- study-spring/src/test/java/kr/springboot/springstudy/ddd/domainservice/OrderServiceTest.java
+++ study-spring/src/test/java/kr/springboot/springstudy/ddd/domainservice/OrderServiceTest.java
@@ -15,14 +15,20 @@ class OrderServiceTest {
     @DisplayName("getDiscountPrice 메소드는")
     class Describe_getDiscountPrice {
 
-        @DisplayName("할인된 가격을 반환한다.")
-        @Test
-        void it_get_discount_price() {
+        @Nested
+        @DisplayName("주문 금액이 10000원 일 경우")
+        class Context_with_order_10000 {
 
-            OrderService orderService = new OrderService(new DiscountCalculationsService());
-            Order order = new Order(10000);
-            int discountPrice = orderService.getDiscountPrice(order);
-            assertThat(discountPrice).isEqualTo(9000);
+            @DisplayName("10% 할인 금액인 9000원을 반환한다.")
+            @Test
+            void it_get_discount_price() {
+
+                OrderService orderService = new OrderService(new DiscountCalculationsService());
+                Order order = new Order(10000);
+                int discountPrice = orderService.getDiscountPrice(order);
+                assertThat(discountPrice).isEqualTo(9000);
+
+            }
 
         }
 
