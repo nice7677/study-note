84d55b2f2c65e010273a6aaa46392acdfbd1d983
진우 <nice7677@naver.com>
2023-09-13 22:37:55 +0900

    string intern

---
+++ java-study/src/main/java/kr/springboot/string/intern/Hello.java
@@ -0,0 +1,17 @@
+package kr.springboot.string.intern;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        String a = "a";
+        String aa = "aa";
+        String aaa = "aaa";
+
+        System.out.println(aaa == (a + aa).intern());
+        System.out.println("aaa" == aaa.intern());
+        System.out.println("aaa".intern() == aaa);
+
+    }
+
+}
